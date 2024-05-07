0e07a573508e0c2ddeccd725f565ac693f57d6ff
Nazarii Dakh <nazarii.dakh@gmail.com>
2024-05-07 16:21:11 +0300

    hmara-bot

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 
 setup(
-    name='signalbot',
+    name='hmara-signalbot',
     version='0.0.1',
     url='https://signal-bot.github.io',
     license='AGPL-3.0',
     packages=['signalbot', 'signalclidbusmock'],
     entry_points={
         'console_scripts': [
-            'signal-bot=signalbot.cli:main',
+            'hmara-signal-bot=signalbot.cli:main',
         ]
     }
 )
